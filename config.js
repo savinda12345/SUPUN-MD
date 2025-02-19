d97b85dd9a830b634796819ba8ad6b58562df641
savinda12345 <sesandasudesh@gmail.com>
2025-02-19 14:54:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'session id', //add your session id
-MONGODB: process.env.MONGODB || "mongodb url", //add your mongodb url
+SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=KQVykQYJ#ezkheLb6n1pw57vam1yL9fHmw85oSGofAX1Hx6jXgp0', //add your session id
+MONGODB: process.env.MONGODB || "mEGsowNoNNvGHwBcJFgCJnSfAaBbPbEU", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
 };
